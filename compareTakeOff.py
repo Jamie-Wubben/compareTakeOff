e1c8814f51c69af6f50da5730d1696d776a4fc85
dewub <jwebber@mcom.disca.upv.es>
2021-02-01 16:32:55 +0100

    removing all experiments and starting anew due to altitude not set consistent to 10 m

--- compareTakeOff.py
+++ compareTakeOff.py
@@ -4,11 +4,12 @@ import os.path
 
 strategies = ["Hungarian","Simplified"]
 groundFormations = ["Random"]
-airFormations = ["Circle"]
+airFormations = ["Regular matrix","Circle","Linear"]
 
 protocolparametersFilePath = "compareTakeOff.properties"
 ardusimParametersFilePath = "SimulationParam.properties"
-logFilePath = "logFileRandomtoCircle.txt"
+logFilePath = "logFile.txt"
+sequential = "false"
 
 def writeProtocolParameters(strategy,ground,air,numUAVs):
     with open(protocolparametersFilePath, "w") as f:
@@ -18,8 +19,8 @@ def writeProtocolParameters(strategy,ground,air,numUAVs):
         f.write("takeOffStrategy=" + strategy + "\n")
         f.write("flyingFormation=" + air + "\n")
         f.write("flyingMinDistance=50\n")
-        f.write("outputFile=randomToCircle.csv\n")
-        f.write("takeOffIsSequential=false")
+        f.write("outputFile=randomToMatrix.csv\n")
+        f.write("takeOffIsSequential=" + str(sequential) + "\n")
         f.write("altitude=10")  
 
 def writeArduSimParameters(numUAVs):
@@ -31,61 +32,25 @@ def writeArduSimParameters(numUAVs):
     with open(ardusimParametersFilePath,'w') as file:
         file.writelines(data)
 
-def getTotalTime(filename):
-    totalTime=0
-    counter=0
-    with open(filename, 'r') as file:
-        allData = file.readlines()
-
-    for data in allData:
-        if data == "\n":
-            continue
-
-        values = data.split(";")
-        time = values[-2] #last item is a \n so -2 to get full time
-        try:
-            totalTime = totalTime + int(time)
-            counter = counter +1
-        except:
-            pass
-  
-    if counter >0:    
-        avarage = totalTime/counter
-        return avarage/1000
-    else:
-        return 1500
-
-def getEstimatedForTimeout(numUAVs):
-    filename = "compareTakeOff" + str(numUAVs) + ".csv"
-    if os.path.isfile(filename):
-        return getTotalTime(filename)+180
-    else:
-        return 1500
-
 def removeFoldersAfterError():
     directories_to_remove=glob.glob('virtual_uav_temp_*')
     for directory in directories_to_remove:
         cmd = ['rm','-r',directory]
         subprocess.run(cmd)
 
-for a in range(1,8):
-    numUAVs = a*25
-    writeArduSimParameters(numUAVs)
+for ground in groundFormations:
     for strategy in strategies:
-        for ground in groundFormations:
-            for air in airFormations:
+        for air in airFormations:
+            for a in range(1,9):
+                numUAVs = a*25
+                writeArduSimParameters(numUAVs)
                 writeProtocolParameters(strategy,ground,air,numUAVs)
                 cmd = ['jdk-13/bin/java','-jar', 'ArduSim.jar', 'simulator-cli', ardusimParametersFilePath]
                 try:
-                    time = getEstimatedForTimeout(numUAVs)
-                    if time <= 1500:
-                        print("timeout is set to " + str(time) + " seconds")
-                        subprocess.run(cmd, timeout=time)
-                        with open(logFilePath, 'a') as file:
-                            file.write(str(numUAVs) + ";" + strategy + ";" + ground + ";" + air + ";executed correctly\n")
-                    else:
-                        with open(logFilePath, 'a') as file:
-                            file.write(str(numUAVs) + ";" + strategy + ";" + ground + ";" + air + ";estimated time is longer than battery cap\n") 
+                    print("running " + str(numUAVs) + " drones going to " + air + " with " + strategy)
+                    subprocess.run(cmd, timeout=600)
+                    with open(logFilePath, 'a') as file:
+                        file.write(str(numUAVs) + ";" + strategy + ";" + ground + ";" + air + ";executed correctly\n")
                 except subprocess.TimeoutExpired:
                     removeFoldersAfterError()
                     with open(logFilePath, 'a') as file:
